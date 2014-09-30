6a74354d1e64e832a3890399196b3bcafdb6df9b
Alejandro Llamas <afllm007@gmail.com>
2014-09-30 17:29:06 +0200

    eso

---
+++ RepasoJavaPSP/src/ejercicio1/Nombre.java
@@ -0,0 +1,18 @@
+package ejercicio1;
+
+import java.util.Scanner;
+
+public class Nombre {
+	
+	private Scanner scn;
+	String nombre;
+	
+	
+	
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
